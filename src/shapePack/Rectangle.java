291ef3bcfebfe31e2a654431b7bb00198f6b8268
Omarelk17 <129680781+Omarelk17@users.noreply.github.com>
2024-11-09 15:35:45 -0600

    Update Rectangle.java

--- src/shapePack/Rectangle.java
+++ src/shapePack/Rectangle.java
@@ -4,4 +4,8 @@ public class Rectangle extends Shape {
     public void printRectangle() {
         System.out.println("This is Rectangle. Subclass of Shape");
     }
+    
+    public double calculateArea(double length, double breadth) {
+        return length * breadth;
+    }
 }
